8f95d5f9320ca0a605bad0dbc96d925418e8f768
Vishal Bhatia <vishalbhatia@Vishals-MBP.lan>
2020-12-03 13:47:27 -0500

    updated

--- src/components/autocompletebox.js
+++ src/components/autocompletebox.js
@@ -4,6 +4,8 @@ import React, { Component } from "react";
 class AutocompleteBox extends Component {
   state = {
     suggested_items: [],
+    cursor:0,
+    backgroundValue:"white",
   };
 
   handleChange = (e) => {
@@ -33,17 +35,53 @@ class AutocompleteBox extends Component {
     this.setState({ value: e.target.value });
   };
 
-  handleKeyDown = (e) => {};
+  /*handleKeyDown = (e) => {
+    if (e.keyCode === 40){
+      if (this.state.cursor <= this.state.suggested_items.length){
+    this.setState({ value: this.state.suggested_items[this.state.cursor]});
+    this.setState({cursor : this.state.cursor + 1});
+      }
+    }
+  };*/
+
+  handleKeyUp = (e) => {
+    if (e.keyCode === 38 && this.state.cursor >= 0){
+      if (this.state.cursor <= this.state.suggested_items.length && this.state.cursor >= 0){
+    this.setState({ value: this.state.suggested_items[this.state.cursor]});
+    this.setState({cursor : this.state.cursor - 1});
+    this.setState({backgroundValue:"grey"});
+      }
+    }
+
+    if (e.keyCode === 38 && this.state.cursor <= 0){
+      this.setState({cursor:0});
+    }
+  };
+
+  handleKeyDown = (e) => {
+    if (e.keyCode === 40 && this.state.cursor <= this.state.suggested_items.length){
+    this.setState({ value: this.state.suggested_items[this.state.cursor] });
+    this.setState({cursor: this.state.cursor + 1});
+    this.setState({backgroundValue:"grey"});
+    }
+
+    if (e.keyCode === 40 && this.state.cursor >= this.state.suggested_items.length){
+      this.setState({cursor: this.state.suggested_items.length - 1 })
+    }
+  };
+
+
+  handleClick = (item) => {
+    this.setState({ value: item });
+  };
 
   render() {
     const inputstyle = {
-      //height: "60px",
       width: "700px",
       fontSize: "36px",
     };
 
     const inputstyle2 = {
-      //height: "60px",
       width: "700px",
       margin: "0px",
       fontSize: "36px",
@@ -52,9 +90,9 @@ class AutocompleteBox extends Component {
 
     const divstyle = {
       margin: "200px",
-      /*textAlign: "center",*/
     };
 
+  
     return (
       <div style={divstyle}>
         <input
@@ -62,12 +100,14 @@ class AutocompleteBox extends Component {
           type="text"
           value={this.state.value}
           onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
+          onKeyUp={this.handleKeyUp}
         />
         <div>
           {this.state.suggested_items.map((item) => {
             return (
-              <div style={inputstyle2} onKeyDown={this.handleKeyDown}>
-                {item}
+              <div style={inputstyle2} onClick={(event) => this.handleClick(item)}>
+                <div style={{background:this.state.backgroundValue}}>{item}</div>
               </div>
             );
           })}
